91c709f158d5c272c644036ad7fda7722b58748e
Jakub Řihák <jakub.rihak@gmail.com>
2018-01-04 10:08:32 +0100

    Added package 'requests' to install requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(name='dsapy',
-      version='0.1',
+      version='0.2',
       description='Python client for DSpace 5.X API',
       url='http://github.com/jrihak/dsapy',
       author='Jakub Řihák',
       license='MIT',
       packages=['dsapy'],
+      install_requires=['requests'],
       zip_safe=False)
