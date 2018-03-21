4a910afacbaecb2e4051c7fe47827b39dddbd42d
Jakub Řihák <jakub.rihak@gmail.com>
2018-03-21 11:59:55 +0100

    Version changed to 0.31

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dsapy',
-      version='0.3',
+      version='0.31',
       description='Python client for DSpace 5.X API',
       url='http://github.com/jrihak/dsapy',
       author='Jakub Řihák',
